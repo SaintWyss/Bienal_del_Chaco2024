68224a7a3f374711f6dc7f2a8c52d9ab262d17a0
santiago <santiagov.scacciaferrow@gmail.com>
2024-11-19 11:56:05 -0300

    fix: corregir el cerrar seccion

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,7 @@
 import AppRouter from './router/router.tsx';
 import { EventProvider } from './features/events/context/EventContext';
-import useAuth from './features/auth/hooks/useAuth';
-import {useEffect} from "react"; // Importa useAuth
 
 function App() {
-    const { fetchUser } = useAuth(); // Usa el hook useAuth
-
-    useEffect(() => {
-        fetchUser(); // Obtén el usuario al iniciar la app
-    }, [fetchUser]);
 
     return (
         <EventProvider>
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
 
-
 const rootElement = document.getElementById('root') as HTMLElement;
 console.log('Variables de entorno:', import.meta.env);
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-
         <App />
     </React.StrictMode>
 );
